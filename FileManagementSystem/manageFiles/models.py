f217d0d0ea88f6f412556cb9cf3859fdd50fe5ec
Lelana99 <lelana2021@gmail.com>
2022-05-23 13:01:41 +0600

    Added a file form

--- FileManagementSystem/manageFiles/models.py
+++ FileManagementSystem/manageFiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class File(models.Model):
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.first_name + ' ' + self.last_name 
--- FileManagementSystem/manageFiles/urls.py
+++ FileManagementSystem/manageFiles/urls.py
@@ -1,6 +1,8 @@
+from inspect import formatargspec
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+   
 ]
