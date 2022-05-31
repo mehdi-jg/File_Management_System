4dc19399b667c682fc7aaaa1b9af12b4a41f08f8
mehdi_rahman <mehdi.jgtdsl@gmail.com>
2022-05-31 10:06:03 +0600

    Added division, department and section models

--- FileManagementSystem/manageFiles/forms.py
+++ FileManagementSystem/manageFiles/forms.py
@@ -5,3 +5,18 @@ class FileForm(forms.ModelForm):
     class Meta:
         model = File
         fields = '__all__'
+        labels = {
+            'file_name': 'File Name',
+            'file_number': 'File Number',
+            'file_division': 'Division',
+            'file_department': 'Department',
+            'file_section': 'Section',
+        }
+    # For Showing text "Select" in designation dropdown list instead "----------"
+
+    def __init__(self, *args, **kwargs):
+        super(FileForm, self).__init__(*args, **kwargs)
+        self.fields["file_division"].empty_label = "Select a Division"
+        self.fields["file_department"].empty_label = "Select a Department"
+        self.fields["file_section"].empty_label = "Select a Section"
+
--- FileManagementSystem/manageFiles/models.py
+++ FileManagementSystem/manageFiles/models.py
@@ -1,11 +1,56 @@
+from asyncio.windows_events import NULL
 from django.db import models
 
 # Create your models here.
 
+class JGDivision(models.Model):
+    DivisionName = models.CharField(max_length=200, blank=True, null=True)
+    NothiCode = models.CharField(max_length=3, blank=True, null=True)
+
+    def __str__(self):
+        return self.DivisionName
+
+
+class JGDepartment(models.Model):
+    DepartmentName = models.CharField(max_length=200, blank=True, null=True)
+    JGDivision = models.ForeignKey(
+        JGDivision, blank=True, null=True, on_delete=models.CASCADE)
+    NothiCode = models.CharField(max_length=3, blank=True, null=True)
+
+    def __str__(self):
+        return self.DepartmentName
+
+
+class JGSection(models.Model):
+    SectionName = models.CharField(max_length=200, blank=True, null=True)
+    JGDepartment = models.ForeignKey(
+        JGDepartment, blank=True, null=True, on_delete=models.CASCADE)
+    NothiCode = models.CharField(max_length=3, blank=True, null=True)
+
+    def __str__(self):
+        return self.SectionName
+
+
+class JGSubSection(models.Model):
+    SubSectionName = models.CharField(max_length=200, blank=True, null=True)
+    JGSection = models.ForeignKey(
+        JGSection, blank=True, null=True, on_delete=models.CASCADE)
+    NothiCode = models.CharField(max_length=3, blank=True, null=True)
+
+    def __str__(self):
+        return self.SubSectionName
+
 
 class File(models.Model):
-	file_name = models.CharField(max_length=200)
-	file_number = models.CharField(max_length=200)
+    file_name = models.CharField(max_length=200, blank=True, null=True)
+    file_number = models.CharField(max_length=200, blank=True, null=True)
+    file_division = models.ForeignKey(
+        JGDivision, blank=True, null=True, on_delete=models.CASCADE)
+    file_department = models.ForeignKey(
+        JGDepartment, blank=True, null=True, on_delete=models.CASCADE)
+    file_section = models.ForeignKey(
+        JGSection, blank=True, null=True, on_delete=models.CASCADE)
+    
 
-	def __str__(self):
-		return self.file_name + ' ' + self.file_number 
+    def __str__(self):
+        return self.file_name
